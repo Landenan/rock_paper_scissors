428b12e3fb83ec08b243ca5be45cde43d242497f
Landenan <124471525+Landenan@users.noreply.github.com>
2025-07-02 19:24:43 +0200

    javascript code written in a row

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
-console.log("Hola");
+function getComputerChoice() {
+    let num = Math.floor(Math.random()*3);
+    if (num === 0) {
+        return("rock");
+    } else if (num === 1) {
+        return("paper");
+    } else {
+        return("scissors");
+    }
+}
+
+function getHumanChoice() {
+    let userChoice = prompt("What's your choice? Rock, Paper or Scissors?");
+    let userLower = userChoice.toLowerCase();
+    return(userLower);
+}
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(getComputerChoice, getHumanChoice) {
+    let think = getHumanChoice();
+    let cpu = getComputerChoice();
+
+    if ((think === "rock" && cpu === "paper") || (think === "paper" && cpu === "scissors") ||
+        (think === "scissors" && cpu === "rock")) {
+            return(0);
+    } else if ((think === "rock" && cpu === "scissors") || (think === "paper" && cpu === "rock") || 
+        (think === "scissors" && cpu === "paper")) {
+            return(1);
+    } else {
+        return(2);
+    }
+}
+
+function playGame(playRound) {
+    for(let i = 0; i < 5; i++) {
+        if (playRound(0)){
+            computerScore += 1;
+            console.log("One round for the computer");
+        } else if (playRound(1)) {
+            humanScore += 1;
+            console.log("Computer bytes the dust!");
+        } else {
+            console.log("It's a tie!");
+        }
+    }
+
+    if (computerScore > humanScore) {
+        console.log("Something is wrong! computer wins!");
+    } else {
+        console.log("Wow! You won!");
+    }
+}
